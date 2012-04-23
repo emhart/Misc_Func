2de57a0463f5aac1c108d35b43c626bedb21f5f6
Ted Hart <edmund.m.hart@gmail.com>
2012-04-22 17:00:39 -0700

    Added slopeBias.R the code that accompanies a blog post

--- NCAAModel/ncaamodel2012.r
+++ NCAAModel/ncaamodel2012.r
@@ -288,6 +288,53 @@ combined.output <- final.output
 
 
 
+########Null model#####
+## run a null and compare it to 
+windat <- read.csv("regwinners.csv")
+nboot <- 10000
+nullmod <- vector()
+for(z in 1:nboot){
+winners <- vector()
+n.right <- vector()
+ff4 <- vector()
+ffwinners <- c(1,63,1)
+count <- 1
+for(i in 1:4){
+  
+  tmp.dat <- subset(windat,windat$region==i)
+  win.dat <- tmp.dat[,5:8]
+  teams <- tmp.dat$index
+  for(k in 1:4){
+    n.games <- length(teams)/2
+    position <- sort(rep(1:n.games,2))
+    
+      for(j in 1:n.games){
+         winners[j] <- sample(teams[position==j],1)
+          }
+    n.right[count] <- sum(winners%in%win.dat[,k])
+    teams <- winners
+    winners <- vector()
+    count <- count  + 1
+    
+    
+  }
+ff4[i] <- teams[1]
+  
+}
 
+w1 <- sample(ff4[1:2],1)
+w2 <- sample(ff4[3:4],1)
+ch <- sample(c(w1,w2),1)
+ch.cor <- sum(c(w1,w2,ch)%in%ffwinners)
+
+nullmod[z] <- sum(n.right)+ch.cor
+}
+
+ranks <- c(8,9,13,8,2)
+
+library(ggplot2)
+nullmod <- data.frame(nullmod)
+ncaa.fig <- ggplot(nullmod,aes(x=nullmod))+geom_density(fill="red",alpha=.3)+ylab("Density")+xlab("Number of games correct")
+ncaa.fig + geom_vline(xintercept=31,linetype=2)+geom_vline(xintercept=40)+geom_vline(xintercept=43,colour="blue")+geom_vline(xintercept=51,colour="red")+opts(axis.text.y=theme_text(size=12))+opts(axis.text.x=theme_text(size=12))
 
 
---
+++ slopeBias.R
@@ -0,0 +1,77 @@
+#####Generate noisy data
+###Create design matrix
+X<- cbind(rep(1,100),rnorm(100,0,4),rnorm(100,0,4),rnorm(100,0,4),rnorm(100,0,4))
+###Set effects sizes
+effs <- c(1,1,1,1,1)
+Y<- X%*%effs
+
+####Full model shows perfect correlation
+full.mod <- lm(Y~X)
+###Choose random covariate, 2 in this case
+m1 <- lm(Y~X[,3])
+my.y <- Y
+my.x <- X[,3]
+
+
+####Now create some output matrices to store the data####
+##Store the r^2 of the original model, the r^2 (correlation) of the between the true 
+## x and the measured x, and the sd of the newly fit model
+sdvars <- seq(.001, 20,length=1000)
+xerr.output <- matrix(0,ncol=5,nrow=1000)
+colnames(xerr.output) <- c("alpha","beta","rsq","cor","sd")
+
+for(i in 1:1000){
+  ####create new variables with measurement error
+  xstar <- my.x + rnorm(100,0,sd=sdvars[i])
+  tmp.m1 <- lm(my.y~xstar)
+  cormod <- lm(xstar~my.x)  
+  ####Collect output
+  xerr.output[i,] <- c(coef(tmp.m1),summary(tmp.m1)$r.sq,summary(cormod)$r.sq,sd(resid(tmp.m1)))
+  
+}
+
+yerr.output <- matrix(0,ncol=5,nrow=1000)
+colnames(yerr.output) <- c("alpha","beta","rsq","cor","sd")
+
+for(i in 1:1000){
+  ####create new variables with measurement error
+  ystar <- my.y + rnorm(100,0,sd=sdvars[i])
+  tmp.m1 <- lm(ystar~my.x)
+  cormod <- lm(ystar~my.y)  
+  ####Collect output
+  yerr.output[i,] <- c(coef(tmp.m1),summary(tmp.m1)$r.sq,summary(cormod)$r.sq,sd(resid(tmp.m1)))
+  
+}
+
+plot(output[,5]~sqrt(output[,4]))
+plot(output[,3]~sqrt(output[,4]))
+plot(output[,2]~sqrt(output[,4]))
+abline(coef(tm1)[2],0)
+
+plot(output[,1]~sqrt(output[,4]))
+abline(coef(tm1)[1],0)
+plot(output[,1:2])
+
+yerr.output <- data.frame(yerr.output)
+xerr.output <- data.frame(xerr.output)
+
+####Code to make first blog figure#####
+plot(my.y~my.x, ylab="Stupid Response",xlab="Silly Question")
+for(i in 1:100){abline(xerr.output[i,1:2],lwd=.5,col=2)}
+for(i in 1:100){abline(yerr.output[i,1:2],lwd=.5,col=4)}
+abline(m1,lty=2,lwd=3)
+
+comb.out <- rbind(yerr.output,xerr.output)
+comb.out$errt <- c(rep("y",1000),rep("x",1000))
+###Code for slope figures
+library(ggplot2)
+#####
+ggplot(comb.out,aes(x=sqrt(cor),y=beta,colour=errt))+geom_point()+scale_colour_discrete(name="Variable \n with error")+xlab("Correlation with true value")+ylab("Slope")+geom_abline(intercept=1.1638,slope=0)
+
+
+###Code for R^2 figure
+ggplot(comb.out,aes(x=sqrt(cor),y=rsq,colour=errt))+geom_point()+scale_colour_discrete(name="Variable \n with error")+xlab("Correlation with true value")+ylab("R-Sqared")+geom_abline(intercept=0.2617,slope=0)
+
+
+
+
